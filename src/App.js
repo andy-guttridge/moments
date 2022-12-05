6e34b7608d9f0077621020d3f4b699499ec16d6d
Andy G <andyguttridge123@gmail.com>
2022-12-05 18:11:32 +0000

    Feature: add bootstrap stylesheet and test button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <Button variant="outline-primary">Primary</Button>
     </div>
   );
 }
