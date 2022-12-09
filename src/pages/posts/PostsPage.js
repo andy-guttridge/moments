80d7d7eb25bb97b83a0688f97d5a5c2d30a87e3a
Andy G <andyguttridge123@gmail.com>
2022-12-09 11:54:52 +0000

    Feature: add posts lists

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -7,20 +7,83 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function PostsPage() {
-  
-  return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
-      </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
-      </Col>
-    </Row>
-  );
+import Post from "./Post";
+
+import NoResults from '../../assets/no-results.png';
+import Asset from "../../components/Asset";
+
+// Destructure props in place
+function PostsPage({ message, filter = "" }) {
+    const [posts, setPosts] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+
+    // The useLocation hook is from the React library, and returns data about the URL the user is currently on.
+    const { pathname } = useLocation();
+
+    // We pass filter and pathname into the useEffect dependency array, so that our code to fetch the posts runs everytime either the filter or pathname is changed.
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                // The filter parameter here comes from the prop we sent in from our <Route> component.
+                const { data } = await axiosReq.get(`/posts/?${filter}`);
+                // Set the posts to the loaded data and hasLoaded to true so that the spinner is not displayed.
+                setPosts(data);
+                setHasLoaded(true);
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+        // Set hasLoaded to false before we fetch the posts, so that the loading spinner is displayed.
+        setHasLoaded(false);
+        fetchPosts();
+    }, [filter, pathname])
+
+    return (
+        <Row className="h-100">
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                <p>Popular profiles mobile</p>
+                <i className={`fas fa-search ${styles.SearchIcon}`} />
+                {/* Our search form submission is handled by an onChange event, rather than onSubmit */}
+                <Form className={styles.SearchBar}
+                    onSubmit={(event) => event.preventDefault()}
+                >
+                    <Form.Control
+                        type="text"
+                        className="mr-sm-2"
+                        placeholder="Search posts"
+                    />
+                </Form>
+                {/* Nested ternaries - have the posts loaded? If yes then display the posts or the no resuts message if none were returned. It not, then show loading spinner. */}
+                {hasLoaded ? (
+                    <>
+                        {console.log("posts found")}
+                        {posts.results.length ? (
+                            posts.results.map(post => (
+                                // Note we spread the post object to provide props to the Post component, and pass it the setPosts function so that users can like/unlike posts.
+                                <Post key={post.id} {...post} setPosts={setPosts} />
+                            ))
+                        ) : (
+                            <Container className={appStyles.Content}>
+                                {/* Use our Asset component, passing in the NoResults image and the message string we've received from the parent component. */}
+                                <Asset src={NoResults} message={message} />
+                            </Container>
+                        )}
+                    </>
+                ) : (
+                    <Container className={appStyles.Content}>
+                        <Asset spinner />
+                    </Container>
+                )}
+            </Col>
+            <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+                <p>Popular profiles for desktop</p>
+            </Col>
+        </Row>
+    );
 }
 
 export default PostsPage;
