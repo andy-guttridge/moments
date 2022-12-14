baec60038eff8e28e602d7cea062256a16d32c13
Andy G <andyguttridge123@gmail.com>
2022-12-14 17:58:21 +0000

    Feature: add PopularProfiles component

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -16,6 +16,7 @@ import NoResults from '../../assets/no-results.png';
 import Asset from "../../components/Asset";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 // Destructure props in place
 function PostsPage({ message, filter = "" }) {
@@ -103,7 +104,7 @@ function PostsPage({ message, filter = "" }) {
                 )}
             </Col>
             <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-                <p>Popular profiles for desktop</p>
+                <PopularProfiles />
             </Col>
         </Row>
     );
